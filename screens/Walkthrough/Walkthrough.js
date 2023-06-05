133b6d864265753617b364dfb66ad26d7f67d7be
I-Chandana <indu.itexphere@gmail.com>
2023-06-05 22:55:35 +0530

    jun05 001

--- screens/Walkthrough/Walkthrough.js
+++ screens/Walkthrough/Walkthrough.js
@@ -8,9 +8,18 @@ import {
 import { TextButton } from "../../components"
 import { COLORS, SIZES, constants, FONTS } from "../../constants"
 import Walkthrough1 from './Walkthrough1';
+import Walkthrough2 from './Walkthrough2';
 
 const Walkthrough = () => {
 
+    // Walkthrough 2
+    const [walkthrough2Animate, setWalkthrough2Animate] = React.useState(false);
+
+    const onViewChangeRef = React.useRef(({ viewableItems, changed }) => {
+        if (viewableItems[0].index == 1) {  // Walkthrough 2 -- 35:32
+            setWalkthrough2Animate(true)
+        }
+    })
     const scrollX = React.useRef(new Animated.Value(0)).current;
 
     const Dots = () => {
@@ -121,6 +130,7 @@ const Walkthrough = () => {
                 decelerationRate="fast"
                 showsHorizontalScrollIndicator={false}
                 scrollEventThrottle={16}
+                onViewableItemsChanged={onViewChangeRef.current}
                 onScroll={Animated.event(
                     [{ nativeEvent: { contentOffset: { x: scrollX } } }],
                     {
@@ -143,6 +153,7 @@ const Walkthrough = () => {
                                 }}
                             >
                                 {index == 0 && <Walkthrough1 />}
+                                {index == 1 && <Walkthrough2 animate={walkthrough2Animate} />}
                             </View>
 
                             {/* Title & Descrription */}
